28a8aa3bf9fb484c2cfe4dd3755cf462b8be0ee9
John Ochs <john.w.ochs0@gmail.com>
2019-09-19 19:55:16 -0500

    finished everything with the exception of tweaking spotify api call

--- liri/liri.js
+++ liri/liri.js
@@ -1,58 +1,118 @@
 require('dotenv').config()
-var sptfy = require('node-spotify-api');
 var axios = require("axios")
 var keys = require("./keys.js");
-// var spotify = Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
 var fs = require('fs');
+console.log(keys.spotify.id)
+var stupidfy = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+})
 
 var comm = process.argv[2];
 var input = process.argv.slice(3).join(" ");
-var q;
-console.log(input);
-console.log(comm);
+var r;
+var t = 0;
+console.log("_______________________________________")
 
-function band() {
-    axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp").then(
+function doreplace(r) {
+    r = r.replace(/spotify-this-song,/g, '')
+    r = r.replace(/concert-this,/g, '')
+    r = r.replace(/movie-this,/g, '')
+    r = r.replace(/,/g, '');
+    r = r.replace(/"/g, '');
+    if (t === 1) {
+        song(r)
+    }
+    else if (t === 2) {
+        band(r)
+    }
+    else if (t === 3) {
+        movie(r)
+    }
+    else if (t === 0) {
+        console.log("?")
+    }
+    else {
+        console.log(t)
+    }
+}
+
+function band(r) {
+    if (r === undefined) {
+        r = input
+    }
+    axios.get("https://rest.bandsintown.com/artists/" + r + "/events?app_id=codingbootcamp").then(
         function (response) {
-            console.log(response)
+            var b = response.data
+            for (var y = 0; y < b.length; y++) {
+                var i = "";
+                var p = "";
+                var a = "";
+                var o = b[y].datetime
+                for (var u = 0; u < o.length; u++) {
+                    if (u < 4) {
+                        i = i + o[u]
+                    }
+                    if (u > 4 && u < 7) {
+                        p = p + o[u]
+                    }
+                    if (u > 7 && u < 10) {
+                        a = a + o[u]
+                    }
+                }
+                console.log(b[y].venue.name, "\n", b[y].venue.city + ", " + b[y].venue.country, "\n", "Date: " + p + "/" + a + "/" + i, "\n")
+            }
+            // console.log(b)
+            console.log("_______________________________________")
         }
     )
 }
 
-function movie() {
-    axios.get("http://www.omdbapi.com/?t=" + input + "&apikey=" + keys.omdb.key).then(
+function movie(r) {
+    if (r === undefined) {
+        r = input
+    }
+    axios.get("http://www.omdbapi.com/?t=" + r + "&apikey=" + keys.omdb.key).then(
         function (response) {
-            console.log(response)
+            var m = response.data;
+            console.log(m.Title, "\n", "Year: " + m.Year, "\n", m.Ratings[0].Source + ": " + m.Ratings[0].Value, "\n", m.Ratings[1].Source + ": " + m.Ratings[1].Value, "\n", m.Country, "\n", m.Language, "\n\n", "Plot: " + m.Plot, "\n\n", "Actors: " + m.Actors)
+            console.log("_______________________________________")
         }
     )
 }
 
 function song() {
-    // sptfy.search()
-    spotify.search()
+    console.log(stupidfy)
+    stupidfy.search({ type: 'track', query: input }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(data.tracks.items[0]);
+    });
 }
 
 function dothis() {
     fs.readFile('random.txt', 'utf-8', function (err, data) {
         if (err) { throw err };
-        q = data;
-        // console.log(q)
+        r = data
+        var q = data;
         var e = ("");
-        var r = ("");
-        var t = false
         for (var w = 0; w < q.length; w++) {
-            e = e += q[w]
-            if (e === "spotify-this-song," || e === "concert-this," || e === "movie-this,") {
-                t = true
+            e = e += r[w]
+            if (e === "spotify-this-song,") {
+                t = 1;
+                doreplace(r)
             }
-            if (t === true) {
-                r = r + q[w]
+            else if (e === "concert-this,") {
+                t = 2;
+                doreplace(r)
+            }
+            else if (e === "movie-this,") {
+                t = 3;
+                doreplace(r)
             }
         }
-        r = r.replace(/,/g, '');
-        r = r.replace(/"/g, '');
-        console.log(r)
-        // console.log(e)
     });
 }
 
